433ee37d40f2fb0dc339b4db254c40909103d960
PeterQiu0516 <peterqiu0516@gmail.com>
2021-10-13 21:32:35 -0400

    Enable inter-server communication from NLP to Backend

--- nlp/nlp/api/nlp_server.py
+++ nlp/nlp/api/nlp_server.py
@@ -1,5 +1,6 @@
 from flask import request
 import nlp
+import requests
 
 @nlp.app.route('/', methods=["GET"])
 def base():
@@ -15,3 +16,17 @@ def receive_email():
 def receive_voice():
     print(request.form.get('filename'))
     return "" # we essentially don't care about the interface of the NLP server
+
+@nlp.app.route('/send/', methods=["GET"])
+def send_command():
+    email_id = -1
+    command = request.args.get("command", default='read', type=str)
+    args = {}
+    command_dict = {
+        "id": email_id,
+        "command": command,
+        "args": args
+    }
+    requests.post(f"http://localhost:{nlp.app.config['BACKEND_SERVER_PORT']}/api/command/", json=command_dict)
+    return command_dict
+    
