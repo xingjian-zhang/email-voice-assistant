7a7d9836a2fa3257dee156df1dc1242eea9d5a54
Nebularaid2000 <987082268@qq.com>
2021-11-18 09:23:01 +0800

    Add dialogflow util and demo

--- nlp/nlp/dialogflow/demo.py
+++ nlp/nlp/dialogflow/demo.py
@@ -1,14 +1,15 @@
 from google.cloud import dialogflow
 import nlp
 import requests
+import datetime
 
 PROJECT_ID = "test-conv-ai-1011"
 LANGUAGE_CODE = "en-US"
 
 class Dialogflow_session:
     def __init__(self, session_id):
-        self.email_id = 0
-        self.email_dict = {}
+        self.email_ids = [0] # list of id
+        self.email_dicts = [] # list of dicts
         # self.init = False # todo
         self.project_id = PROJECT_ID
         self.session_id = session_id
@@ -20,17 +21,18 @@ class Dialogflow_session:
 
     def _build_session(self):
         self.session_client = dialogflow.SessionsClient.from_service_account_json(
-            './test-conv-ai-1011-3b1d693b53da.json')  # todo: need a more reasonable way to hardcode the file path
+            '../token/test-conv-ai-1011-3b1d693b53da.json')  # todo: need a more reasonable way to hardcode the file path
 
         self.session = self.session_client.session_path(self.project_id, self.session_id)
         print("Session path: {}\n".format(self.session))
 
 
     def parse_command_dialogflow(self, text):
+        # print("get user text")
         action, parameters, fulfill_text = self._detect_intent_texts(text)
         args = self._parse(action, parameters)
 
-        return action, self.email_id, args, fulfill_text
+        return action, self.email_ids, args, fulfill_text
 
 
     def _detect_intent_texts(self, text):
@@ -38,11 +40,13 @@ class Dialogflow_session:
 
         query_input = dialogflow.QueryInput(text=text_input)
 
+        # print("send query to df")
+
         response = self.session_client.detect_intent(
             request={"session": self.session, "query_input": query_input}
         )
 
-        # intent = response.query_result.intent.displayName
+        # print(response.query_result)
         action = response.query_result.action  # string
         parameters = response.query_result.parameters  # dict
         fulfill_text = response.query_result.fulfillment_text  # string, this is the response text
@@ -61,7 +65,11 @@ class Dialogflow_session:
                 pass
             elif mode == "time":
                 date = parameters["date-time"][:len("0000-00-00")]
-                query = f"after:{date} before:{date}" # exactly this day
+                date_split = date.split('-')
+                date = datetime.date(int(date_split[0]), int(date_split[1]), int(date_split[2]))
+                next_day = date + datetime.timedelta(days=1)
+                # query = f"after:{date.isoformat()} before:{next_day.isoformat()}" # query exactly this day
+                query = f"from:jimmyzxj@umich.edu" # query exactly this day
                 self._query_backend_and_get_email(query)
             elif mode == "confirm":
                 pass
@@ -80,10 +88,11 @@ class Dialogflow_session:
         email_id = 0  # useless, only a placeholder
         args = {"query":query}
         response = _send_command(commmand, email_id, args) # dict
+        print(response)
 
         # get email id and email content
-        self.email_id = response["id"]
-        self.email_dict = response
+        self.email_ids = [email_dict["id"] for email_dict in response]
+        self.email_dicts = response
 
 
 def _send_command(command, email_id, args):
@@ -93,7 +102,7 @@ def _send_command(command, email_id, args):
         "args": args
     }
     response = requests.get(
-        f"http://localhost:{nlp.app.config['BACKEND_SERVER_PORT']}/api/command/", json=command_dict)
+        f"http://localhost:{3000}/api/command/", json=command_dict)
     # print(response)
     data = response.json()
     # print(data)
@@ -105,3 +114,26 @@ def dict_is_empty(d):
         if v != '':
             return True
     return False
+
+
+df_session = Dialogflow_session(session_id=1234)
+while True:
+    user_text = input("Enter user text: ")
+    # print("user text: ", user_text)
+    if user_text == "quit":
+        break
+    action, email_ids, args, fulfill_text = df_session.parse_command_dialogflow(user_text)
+    print("action: ", action)
+    print("email ids: ", email_ids)
+    print("args: ", args)
+    print("bot text: ", fulfill_text)
+    print('-'*20)
+
+    if action.split('.')[1] == "mark_as_read":
+        command = "read"
+        for email_id in email_ids:
+            _send_command(command, email_id, args)
+    else:
+        command = "default"
+
+
