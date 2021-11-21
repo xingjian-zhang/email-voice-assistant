895179c18b6d2495053d060aab3fdd587965d21b
Nebularaid2000 <987082268@qq.com>
2021-11-21 10:05:53 +0800

    Integrate dialogflow into nlp_server

--- nlp/nlp/__init__.py
+++ nlp/nlp/__init__.py
@@ -7,5 +7,6 @@ from nlp.config import NLP_SERVER_PORT, BACKEND_SERVER_PORT
 # app is a single object used by all the code modules in this package
 app = flask.Flask("nlp")  # pylint: disable=invalid-name
 app.config.from_object('nlp.config')
+app.config["SECRET_KEY"] = "voicemail"
 
 import nlp.api
--- nlp/nlp/api/__init__.py
+++ nlp/nlp/api/__init__.py
@@ -1,2 +1,2 @@
-from nlp.api.nlp_server import base, get_response
+from nlp.api.nlp_server import base, get_response, Dialogflow_session
 from nlp.api.summarize import Summarize
