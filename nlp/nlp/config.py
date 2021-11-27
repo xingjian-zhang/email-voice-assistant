f425165190df90003190e56a8de992c709439a6e
Xingjian Zhang <jimmyzxj@umich.edu>
2021-11-26 20:59:09 -0500

    fix #62 #63 (#64)
    * fix #62 #63
    
    * Delete recipient in the bot response
    
    Co-authored-by: Nebularaid2000 <987082268@qq.com>

--- nlp/nlp/config.py
+++ nlp/nlp/config.py
@@ -5,3 +5,4 @@ import pathlib
 BACKEND_ROOT = pathlib.Path(__file__).resolve().parent.parent
 NLP_SERVER_PORT = 3001
 BACKEND_SERVER_PORT = 3000
+PRIVATE_KEY = pathlib.Path(__file__).resolve().parent / "dialogflow" / "private_key" / "test-conv-ai-1011-3b1d693b53da.json"
